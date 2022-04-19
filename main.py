f69df0d47e5e4784751a5c9e9de3429a08aa3697
windsonleo <windsor_melo@hotmail.com>
2022-04-18 23:35:02 -0300

    TELA INICIAL ABRINDO

--- main.py
+++ main.py
@@ -2,15 +2,23 @@
 
 # Press Shift+F10 to execute it or replace it with your code.
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from view.sushi import Ui_main
+import sys
+from PyQt5.QtWidgets import QMainWindow, QApplication
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class MinhaJanelaMain(QMainWindow, Ui_main):
+
+    def __init__(self, parent=None):
+        super(MinhaJanelaMain, self).__init__(parent)
+        self.setupUi(self)
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    app = QApplication(sys.argv)
+    myWin = MinhaJanelaMain()
+    myWin.show()
+    sys.exit(app.exec_())
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
